2709fce67d9e9d5c2da6b906093f5923ce456e4f
Kanstantsin Downar <downakan@fit.cvut.cz>
2025-03-06 21:02:06 +0100

    BlogGenerator class partially implemented

---
+++ llm_blog_generator/src/blog_generator.py
@@ -0,0 +1,114 @@
+from src.models_setup import gemini_2_flash, BlogClassification
+from src.text_extraction import *
+from src.prompts import prompt_five_shots
+from src.config import CLASSIFICATION_MAP
+
+def get_examples():
+    """"""
+    example_files = {
+        "excellent_blog": "../data/excellent_blog",
+        "very_good_blog": "../data/very_good_blog",
+        "good_blog": "../data/good_blog",
+        "average_blog": "../data/average_blog",
+        "bad_blog": "../data/bad_blog"
+    }
+    examples = {}
+    for key, blog_path in example_files.items():
+        try:
+            with open(blog_path, "r", encoding="utf-8") as file:
+                examples[key] = file.read()
+        except FileNotFoundError:
+            print(f"Error: File {blog_path} not found.")
+            examples[key] = ""
+    return examples
+
+class RateLimiter:
+    """Rate limits: 15 RPM, 10^6 TPM, 1 500 RPD"""
+    def __init__(self,):
+        self.__token_usage = 0
+
+    def wait(self):
+        """"""
+        pass
+
+    def update(self, usage_metadata):
+        """"""
+        pass
+
+class BlogGenerator:
+    """Generate engagement blog from scientific paper"""
+    def __init__(self,
+                 generator=gemini_2_flash, evaluator=gemini_2_flash,
+                 min_engagement_level="Good", max_retries=3, max_retries_call=3):
+        #self.__generator = None
+        #self.__generator_init_prompt = None
+        self.__generator_retry_prompt = None
+        self.__evaluator = evaluator.with_structured_output(BlogClassification, include_raw=True)
+        self.__evaluator_prompt = prompt_five_shots
+        self.__min_engagement_level = min_engagement_level
+        self.__max_retries = max_retries
+        self.__max_retries_call = max_retries_call
+        self.__rate_limiter = RateLimiter()
+
+    def refine_prompt(self, engagement_level, possible_improvements):
+        """"""
+        return None
+
+    def generate_blog(self, paper_url):
+        """"""
+        paper_text = extract_paper_text(paper_url)
+        examples = get_examples()
+        retries = 0
+        best_blog, blog = None, None
+        best_engagement_level, engagement_level = "Bad", None
+        possible_improvements = None
+
+        # generation_chain = self.__generator_init_prompt | self.__generator
+        evaluation_chain = self.__evaluator_prompt | self.__evaluator
+
+        while retries < self.__max_retries:
+            print(f"Attempt number {retries + 1}: Generating blog...")
+
+            generator_response = None
+            for attempt in range(self.__max_retries_call):
+                try:
+                    #if retries >= 1:
+                    #    generation_chain = self.__generator_retry_prompt | generator_structured
+                    #    generator_response = generation_chain.invoke({})
+                    #else:
+                    #    generator_response = generation_chain.invoke({})
+                    if generator_response:
+                        break
+                except Exception as e:
+                    print(f"{e}\nError: Failed to get valid response after {attempt + 1} attempts.\nRetrying...")
+
+            evaluator_response = None
+            for attempt in range(self.__max_retries_call):
+                try:
+                    evaluator_response = evaluation_chain.invoke({
+                        "blog_text" : blog,
+                        **examples
+                    })
+                    if evaluator_response:
+                        break
+                except Exception as e:
+                    print(f"{e}\nError: Failed to get valid response after {attempt + 1} attempts.\nRetrying...")
+
+            usage_metadata = evaluator_response["raw"].usage_metadata
+            self.__rate_limiter.update(usage_metadata)
+
+            content = evaluator_response["parsed"]
+            engagement_level = content.overall_assessment
+            possible_improvements = content.possible_improvements
+            if engagement_level in ["Good", "Very Good", "Excellent"]:
+                return blog
+
+            if CLASSIFICATION_MAP[engagement_level] >= CLASSIFICATION_MAP[best_engagement_level]:
+                best_engagement_level = engagement_level
+                best_blog = blog
+
+            print(f"Blog generation after attempt number {retries + 1} was unsuccessful. Repeat generation...")
+            self.__generator_retry_prompt = self.refine_prompt(engagement_level, possible_improvements)
+            retries += 1
+
+        return best_blog
