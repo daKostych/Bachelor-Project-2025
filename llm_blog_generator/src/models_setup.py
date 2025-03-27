7dd4725f8e5934fbd27f5e07848a5918c00260fa
Kanstantsin Downar <downakan@fit.cvut.cz>
2025-03-27 18:44:37 +0100

    Add: output format

--- llm_blog_generator/src/models_setup.py
+++ llm_blog_generator/src/models_setup.py
@@ -21,6 +21,15 @@ class BlogClassification(BaseModel):
         Field(..., description="Overall assessment of the blog"))
     improvements: List[str] = Field(..., description="Suggested improvements for the blog")
 #=======================================================================================================================
+class BlogClassificationCoT(BaseModel):
+    """Evaluation of a blog post based on detailed analysis and reasoning steps"""
+    blog_title: str = Field(..., description="Blog title")
+    chain_of_thoughts: str = (
+        Field(..., description="Step-by-step analysis and reasoning for the evaluation of the blog post"))
+    overall_assessment: Literal["Excellent", "Very Good", "Good", "Average", "Bad"] = (
+        Field(..., description="Overall assessment of the blog"))
+    improvements: List[str] = Field(..., description="Suggested improvements for the blog")
+#=======================================================================================================================
 class BlogEvaluation(BaseModel):
     """Comprehensive blog assessment on a scale from 1 to 100"""
     blog_title: str = Field(..., description="Blog title")
