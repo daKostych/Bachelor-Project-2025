6e2e15963464ad7d953e35990942f5fe4d61b8c1
Kanstantsin Downar <downakan@fit.cvut.cz>
2025-03-28 21:45:38 +0100

    Add: output formats

--- llm_blog_generator/src/models_setup.py
+++ llm_blog_generator/src/models_setup.py
@@ -1,42 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains import LLMChain
 from langchain_core.rate_limiters import InMemoryRateLimiter
-from langchain_core.pydantic_v1 import BaseModel, Field
-from typing import List, Literal
 from src.config import GEMINI_2_FLASH, GEMINI_2_FLASH_TEMPERATURE, RATE_LIMIT_RPS, RATE_LIMIT_BUCKET
-#=======================================================================================================================
-# Do not work
-"""class CriterionEvaluation(BaseModel):
-    \"""Rating a blog based on one criterion\"""
-    criterion: str = Field(..., description="Name of the criterion")
-    classification: Literal["Excellent", "Very Good", "Good", "Average", "Bad"] = (
-        Field(..., description="Categorical classification of the blog based on the given criterion"))
-    comment: str = Field(..., description="Short comment on the blog's assessment based on a given criterion")"""
-#=======================================================================================================================
-class BlogClassification(BaseModel):
-    """Comprehensive blog classification"""
-    blog_title: str = Field(..., description="Blog title")
-    assessment_explanation: str = Field(..., description="Explanation of the assessment")
-    overall_assessment: Literal["Excellent", "Very Good", "Good", "Average", "Bad"] = (
-        Field(..., description="Overall assessment of the blog"))
-    improvements: List[str] = Field(..., description="Suggested improvements for the blog")
-#=======================================================================================================================
-class BlogClassificationCoT(BaseModel):
-    """Evaluation of a blog post based on detailed analysis and reasoning steps"""
-    blog_title: str = Field(..., description="Blog title")
-    chain_of_thoughts: str = (
-        Field(..., description="Step-by-step analysis and reasoning for the evaluation of the blog post"))
-    overall_assessment: Literal["Excellent", "Very Good", "Good", "Average", "Bad"] = (
-        Field(..., description="Overall assessment of the blog"))
-    improvements: List[str] = Field(..., description="Suggested improvements for the blog")
-#=======================================================================================================================
-class BlogEvaluation(BaseModel):
-    """Comprehensive blog assessment on a scale from 1 to 100"""
-    blog_title: str = Field(..., description="Blog title")
-    assessment_explanation: str = Field(..., description="Explanation of the assessment")
-    overall_assessment: float = (
-        Field(..., description="Overall assessment of the blog on a scale from 1.0 to 100.0"))
-    improvements: List[str] = Field(..., description="Suggested improvements for the blog")
 #=======================================================================================================================
 # API request limit
 rate_limiter = InMemoryRateLimiter(
---
+++ llm_blog_generator/src/output_formats.py
@@ -0,0 +1,59 @@
+from pydantic import BaseModel, Field
+from typing import List, Literal
+#=======================================================================================================================
+# Do not work
+"""class CriterionEvaluation(BaseModel):
+    \"""Rating a blog based on one criterion\"""
+    criterion: str = Field(..., description="Name of the criterion")
+    classification: Literal["Excellent", "Very Good", "Good", "Average", "Bad"] = (
+        Field(..., description="Categorical classification of the blog based on the given criterion"))
+    comment: str = Field(..., description="Short comment on the blog's assessment based on a given criterion")"""
+#=======================================================================================================================
+class BlogEvaluation(BaseModel):
+    """Comprehensive blog assessment on a scale from 1 to 100"""
+    blog_title: str = Field(..., description="Blog title")
+    assessment_explanation: str = Field(..., description="Explanation of the assessment")
+    overall_assessment: float = (
+        Field(..., description="Overall assessment of the blog on a scale from 1.0 to 100.0"))
+    improvements: List[str] = Field(..., description="Suggested improvements for the blog")
+#=======================================================================================================================
+class BlogClassification(BaseModel):
+    """Comprehensive blog classification"""
+    blog_title: str = Field(..., description="Blog title")
+    assessment_explanation: str = Field(..., description="Explanation of the assessment")
+    overall_assessment: Literal["Excellent", "Very Good", "Good", "Average", "Bad"] = (
+        Field(..., description="Overall assessment of the blog"))
+    improvements: List[str] = Field(..., description="Suggested improvements for the blog")
+#=======================================================================================================================
+class BlogClassificationCoT(BaseModel):
+    """Evaluation of a blog post based on detailed analysis and reasoning steps"""
+    blog_title: str = Field(..., description="Blog title")
+    chain_of_thoughts: str = (
+        Field(..., description="Step-by-step analysis and reasoning for the evaluation of the blog post"))
+    overall_assessment: Literal["Excellent", "Very Good", "Good", "Average", "Bad"] = (
+        Field(..., description="Overall assessment of the blog"))
+    improvements: List[str] = Field(..., description="Suggested improvements for the blog")
+#=======================================================================================================================
+class BlogClassificationGK(BaseModel):
+    """Evaluation of a blog post based on previous generated knowledge."""
+    blog_title: str = Field(..., description="Blog title")
+    generated_knowledge: str = (
+        Field(..., description="A key analysis related to evaluating the blog"))
+    overall_assessment: Literal["Excellent", "Very Good", "Good", "Average", "Bad"] = (
+        Field(..., description="Overall assessment of the blog"))
+    improvements: List[str] = Field(..., description="Suggested improvements for the blog")
+#=======================================================================================================================
+class BlogClassificationMP(BaseModel):
+    """Evaluation of a blog post based on detailed analysis of criteria"""
+    blog_title: str = Field(..., description="Blog title")
+    meta_steps: str = (
+        Field(..., description="Step-by-step analysis and reasoning for each criterion"))
+    overall_assessment: Literal["Excellent", "Very Good", "Good", "Average", "Bad"] = (
+        Field(..., description="Overall assessment of the blog"))
+    improvements: List[str] = Field(..., description="Suggested improvements for the blog")
+#=======================================================================================================================
+class BlogGeneration(BaseModel):
+    """Generation engagement blog post form scientific paper"""
+    blog_title: str = Field(..., description="Blog title")
+    text: str = Field(..., description="The full text of the blog post")
+#=======================================================================================================================
