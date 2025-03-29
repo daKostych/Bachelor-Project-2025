b7bfbb7dc927099ab78fac8f78d1c61e0be96b14
Kanstantsin Downar <downakan@fit.cvut.cz>
2025-03-29 21:49:57 +0100

    Add: embedding models

--- llm_blog_generator/src/models_setup.py
+++ llm_blog_generator/src/models_setup.py
@@ -1,7 +1,10 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains import LLMChain
 from langchain_core.rate_limiters import InMemoryRateLimiter
-from src.config import GEMINI_2_FLASH, GEMINI_2_FLASH_TEMPERATURE, RATE_LIMIT_RPS, RATE_LIMIT_BUCKET
+from langchain_huggingface import HuggingFaceEmbeddings
+from sentence_transformers import SentenceTransformer
+from src.config import (GEMINI_2_FLASH, GEMINI_2_FLASH_TEMPERATURE, RATE_LIMIT_RPS,
+                        RATE_LIMIT_BUCKET, EMBEDDING_MODEL_NAME)
 #=======================================================================================================================
 # API request limit
 rate_limiter = InMemoryRateLimiter(
@@ -15,4 +18,8 @@ gemini_2_flash = ChatGoogleGenerativeAI(
     temperature=GEMINI_2_FLASH_TEMPERATURE,
     rate_limiter=rate_limiter
 )
+#=======================================================================================================================
+# Embeddings model
+embedding_model = SentenceTransformer(EMBEDDING_MODEL_NAME)
+langchain_embedding_model = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
 #=======================================================================================================================
