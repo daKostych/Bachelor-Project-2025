4890d7e68255f577944d0b53e515ecab4247e559
Kanstantsin Downar <downakan@fit.cvut.cz>
2025-04-02 00:06:40 +0200

    Add: RPS calculation and models from Gemini family

--- llm_blog_generator/src/models_setup.py
+++ llm_blog_generator/src/models_setup.py
@@ -3,20 +3,42 @@ from langchain.chains import LLMChain
 from langchain_core.rate_limiters import InMemoryRateLimiter
 from langchain_huggingface import HuggingFaceEmbeddings
 from sentence_transformers import SentenceTransformer
-from src.config import (GEMINI_2_FLASH, GEMINI_2_FLASH_TEMPERATURE, RATE_LIMIT_RPS,
+
+from src.config import (GEMINI_2_FLASH, TEMPERATURE, GEMINI_2_FLASH_RPM, GEMINI_2_FLASH_LITE, GEMINI_2_FLASH_LITE_RPM,
+                        GEMINI_2_FLASH_THINKING_EXPERIMENTAL_01_21, GEMINI_2_FLASH_THINKING_EXPERIMENTAL_01_21_RPM,
                         RATE_LIMIT_BUCKET, EMBEDDING_MODEL_NAME)
 #=======================================================================================================================
-# API request limit
-rate_limiter = InMemoryRateLimiter(
-    requests_per_second=RATE_LIMIT_RPS,
-    max_bucket_size=RATE_LIMIT_BUCKET,
-)
+def calculate_rps(rpm):
+    return 1 / (60 / rpm)
 #=======================================================================================================================
-# Initializing the LLM model
+# Gemini 2.0 Flash
 gemini_2_flash = ChatGoogleGenerativeAI(
     model=GEMINI_2_FLASH,
-    temperature=GEMINI_2_FLASH_TEMPERATURE,
-    rate_limiter=rate_limiter
+    temperature=TEMPERATURE,
+    rate_limiter=InMemoryRateLimiter(
+        requests_per_second=calculate_rps(GEMINI_2_FLASH_RPM),
+        max_bucket_size=RATE_LIMIT_BUCKET,
+    )
+)
+#=======================================================================================================================
+# Gemini 2.0 Flash-Lite
+gemini_2_flash_lite = ChatGoogleGenerativeAI(
+    model=GEMINI_2_FLASH_LITE,
+    temperature=TEMPERATURE,
+    rate_limiter=InMemoryRateLimiter(
+        requests_per_second=calculate_rps(GEMINI_2_FLASH_LITE_RPM),
+        max_bucket_size=RATE_LIMIT_BUCKET,
+    )
+)
+#=======================================================================================================================
+# Gemini 2.0 Flash Thinking Experimental 01-21
+gemini_2_flash_thinking = ChatGoogleGenerativeAI(
+    model=GEMINI_2_FLASH_THINKING_EXPERIMENTAL_01_21,
+    temperature=TEMPERATURE,
+    rate_limiter=InMemoryRateLimiter(
+        requests_per_second=calculate_rps(GEMINI_2_FLASH_THINKING_EXPERIMENTAL_01_21_RPM),
+        max_bucket_size=RATE_LIMIT_BUCKET,
+    )
 )
 #=======================================================================================================================
 # Embeddings model
