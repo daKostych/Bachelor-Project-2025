b86e65608c2fc6fab8df9806df8bdbfb676e38b6
Kanstantsin Downar <downakan@fit.cvut.cz>
2025-04-13 00:27:06 +0200

    Add: tmp pdf path

--- llm_blog_generator/src/config.py
+++ llm_blog_generator/src/config.py
@@ -4,17 +4,25 @@ random_seed = 999
 
 BASE_DIR = Path().resolve().parent
 
+TMP_PDF_PATH = BASE_DIR / "tmp" / "temp_paper.pdf"
+
 DEEPMIND_BLOG_DATASET_PATH = BASE_DIR / "data" / "datasets" / "deepmind_blogs.csv"
 PREPROCESSED_DEEPMIND_BLOG_DATASET_PATH = BASE_DIR / "data" / "datasets" / "preprocessed_deepmind_blogs.csv"
 BLOG_DATASET_PATH = BASE_DIR / "data" / "datasets" / "medium_blogs.csv"
 PREPROCESSED_BLOG_DATASET_PATH = BASE_DIR / "data" / "datasets" / "preprocessed_medium_blogs.csv"
 PAPER_DATASET_PATH = BASE_DIR / "data" / "datasets" / "papers.csv"
 PREPROCESSED_PAPER_DATASET_PATH = BASE_DIR / "data" / "datasets" / "preprocessed_papers.csv"
+
 CHROMEDRIVER_PATH = BASE_DIR / "chromedriver-linux64/chromedriver"
+
 EXAMPLES_PATH = BASE_DIR / "data" / "examples"
+
 VECTOR_STORE_PATH = BASE_DIR / "data" / "vector_store"
+
 LONG_TERM_MEMORY_PATH = BASE_DIR / "data" / "long_term_memory"
+
 RESULTS_PATH = BASE_DIR / "data" / "generation_results"
+
 NUMERIC_RESULTS_PATH = BASE_DIR / "data" / "experiment_results" / "numeric_evaluation.csv"
 NORMALIZED_NUMERIC_RESULTS_PATH = BASE_DIR / "data" / "experiment_results" / "normalized_numeric_evaluation.csv"
 CLASSIFICATION_ACC_RESULTS_PATH = BASE_DIR / "data" / "experiment_results" / "classification_acc.csv"
