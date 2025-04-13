c4b1a509972275e3c16d6ac2fe2522d28970a1e5
Kanstantsin Downar <downakan@fit.cvut.cz>
2025-04-13 11:19:16 +0200

    Change: import structure

--- llm_blog_generator/src/text_extraction.py
+++ llm_blog_generator/src/text_extraction.py
@@ -1,6 +1,6 @@
 import time
-import requests
 
+import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
